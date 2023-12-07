56a8036f4481992232345d1c6fb080ec82693620
Aruche-code <117631982+Aruche-code@users.noreply.github.com>
2023-12-07 15:01:22 +0900

    Update route.ts
    testUserをuserに戻しました

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: Request) {
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const user = await prisma.testUser.create({
+    const user = await prisma.user.create({
       data: {
         email,
         name,
