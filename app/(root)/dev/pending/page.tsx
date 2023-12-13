7a2aaf8123e25ef07450b9d07579ab3c56cea8a6
Kanon74 <kd1320455@st.kobedenshi.ac.jp>
2023-12-13 11:32:37 +0900

    学生カルテ一覧の検索、承認待ち、hover

--- app/(root)/dev/pending/page.tsx
+++ app/(root)/dev/pending/page.tsx
@@ -48,26 +48,74 @@ const Pending = () => {
             time3: '10:00~13:00'
         }
     ];
+    const testUsers2 = [
+        {
+            id: '601b92ee95861639c3e2c44b',
+            kana: 'コウベゴロウ',
+            name: '神戸五郎',
+            day1: '2023/12/3',
+            time1: '10:00~12:00',
+            day2: '2023/12/4',
+            time2: '10:00~12:00',
+            day3: '2023/12/5',
+            time3: '10:00~13:00'
+        }
+    ];
 
     return (
-        <div className="flex flex-wrap items-center justify-center text-xs md:text-base">
-            {testUsers.map(user => (
-                <div className="w-5px mx-3">
-                    <Link href="../dev/record" key={user.id}>
-                        <button className="mt-5 p-3 px-24 w-full border-2 border-gray-100 shadow-md rounded-lg">
-                            <div className="text-base md:text-xl">
-                                {user.name}<br />
-                            </div>
 
-                            1.　{user.day1}　{user.time1}<br />
-                            2.　{user.day2}　{user.time2}<br />
-                            3.　{user.day3}　{user.time3}<br />
+        <div className="flex flex-wrap text-xs md:text-base">
+            <div className="flex flex-col mt-10 w-full lg:w-1/2">
 
-                        </button>
-                    </Link>
+                <div className="bg-blue-400 p-2 mx-4 border-4 border-blue-400 rounded-lg text-white">
+                    指名あり
                 </div>
-            ))}
+                {testUsers.map(user => (
+                    <div className="mt-2">
+                        <Link href="../dev/record" key={user.id}>
+                            <div className="flex items-center justify-center">
+                                <button className="mt-5 p-3 w-5/6 border-2 border-gray-100 shadow-md rounded-lg hover:border-4 hover:border-blue-300">
+                                    <div className="text-base md:text-xl">
+                                        {user.name}<br />
+                                    </div>
+
+                                    1.　{user.day1}　{user.time1}<br />
+                                    2.　{user.day2}　{user.time2}<br />
+                                    3.　{user.day3}　{user.time3}<br />
+
+                                </button>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+
+            <div className="flex flex-col mt-10 w-full lg:w-1/2">
+
+                <div className="bg-orange-300 p-2 mx-4 border-4 border-orange-300 rounded-lg text-white">
+                    指名なし
+                </div>
+                {testUsers2.map(user => (
+                    <div className="mt-2">
+                        <Link href="../dev/record" key={user.id}>
+                            <div className="flex items-center justify-center">
+                                <button className="mt-5 p-3 w-5/6 border-2 border-gray-100 shadow-md rounded-lg hover:border-4 hover:border-orange-200">
+                                    <div className="text-base md:text-xl">
+                                        {user.name}<br />
+                                    </div>
+
+                                    1.　{user.day1}　{user.time1}<br />
+                                    2.　{user.day2}　{user.time2}<br />
+                                    3.　{user.day3}　{user.time3}<br />
+
+                                </button>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
+
     );
 
 }
