d35650e191469a025c96e9137c1d4322e6a9f047
Aruche-code <mo.crew2435@gmail.com>
2023-12-15 11:04:02 +0900

    新機能追加と削除

--- app/components/sidebar/StudentSidebar.tsx
+++ app/components/sidebar/StudentSidebar.tsx
@@ -1,35 +1,37 @@
 "use client";
 // 生徒用サイドバーです
 import { useState } from "react";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import HomeIcon from "@mui/icons-material/Home";
 import BadgeIcon from "@mui/icons-material/Badge";
 import CalendarTodaySharpIcon from "@mui/icons-material/CalendarTodaySharp";
-import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
   const pathname = usePathname(); // アクティブなメニューを追跡するための状態
 
   const Menus = [
-    { title: "home", icon: <HomeIcon />, link: "/student", gap: false },
+    { title: "ホーム", icon: <HomeIcon />, link: "/student", gap: false },
     {
-      title: "plofile",
+      title: "プロフィール",
       icon: <BadgeIcon />, //適切なアイコンに変えてください
       link: "/student/profile",
       gap: false,
     },
     {
-      title: "booking",
+      title: "予約",
       icon: <CalendarTodaySharpIcon />, //適切なアイコンに変えてください
       link: "/student/booking",
       gap: false,
     },
+
     {
-      title: "setting",
-      icon: <SettingsIcon />, //適切なアイコンに変えてください
-      link: "/student/setting",
+      title: "ログアウト",
+      icon: <LogoutIcon />, // ログアウトアイコン
+      action: () => signOut(), // ログアウト機能
       gap: true,
     },
   ];
@@ -39,18 +41,39 @@ const Sidebar = () => {
       <div className="flex">
         <div
           className={`${
-            open ? "w-40" : "w-20"
+            open ? "w-46" : "w-20"
           } h-screen p-5 pt-8 relative duration-300 bg-kd-sub2-cl`}
           onMouseEnter={() => setOpen(true)}
           onMouseLeave={() => setOpen(false)}
         >
           <ul className="pt-6">
-            {Menus.map((menu, index) => (
-              <Link href={menu.link} key={index}>
+            {Menus.map((menu, index) =>
+              menu.link ? (
+                <Link href={menu.link} key={index}>
+                  <li
+                    className={`flex rounded-md p-2 cursor-pointer hover:bg-white text-kd-button-cl text-sm items-center gap-x-4
+                  ${menu.gap ? "mt-9" : "mt-2"} 
+                  ${pathname === menu.link && " bg-white"}`}
+                    style={{ whiteSpace: "nowrap", overflow: "hidden" }} // 日本語対応
+                  >
+                    {menu.icon}
+                    <span
+                      className={`${
+                        !open && "hidden"
+                      } origin-left duration-200`}
+                    >
+                      {menu.title}
+                    </span>
+                  </li>
+                </Link>
+              ) : (
                 <li
+                  key={index}
+                  onClick={menu.action} // アクションを追加
                   className={`flex rounded-md p-2 cursor-pointer hover:bg-white text-kd-button-cl text-sm items-center gap-x-4
                   ${menu.gap ? "mt-9" : "mt-2"} 
                   ${pathname === menu.link && " bg-white"}`}
+                  style={{ whiteSpace: "nowrap", overflow: "hidden" }} // 日本語対応
                 >
                   {menu.icon}
                   <span
@@ -59,8 +82,8 @@ const Sidebar = () => {
                     {menu.title}
                   </span>
                 </li>
-              </Link>
-            ))}
+              )
+            )}
           </ul>
         </div>
       </div>
