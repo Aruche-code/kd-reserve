e9634a3da5cc9419627d7e1169953205caf5f99c
Aruche-code <mo.crew2435@gmail.com>
2023-12-19 09:27:26 +0900

    型定義

--- app/components/types.ts
+++ app/components/types.ts
@@ -1,77 +1,24 @@
-export type User = {
-  id: string;
-  name: string | null;
-  email: string | null;
-  role: string | null;
-  emailVerified: Date | null;
-  image: string | null;
-  hashedPassword: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-  studentProfile?: StudentProfile | null;
-  accounts: Account[];
-};
+//-----プロフィールページで使用しています。--------------------------------------------------------------
+// プロフィールStudentProfileの定義
+export interface StudentProfile {
+  department: string;
+  schoolYear: string;
+  tel: string;
+  graduationYear: string;
+  qualification: string;
+  workLocation: string;
+}
 
-export type StudentProfile = {
-  id: string;
-  department: string | null;
-  schoolYear: string | null;
-  tel: string | null;
-  graduationYear: string | null;
-  qualification: string | null;
-  workLocation: string | null;
-  user: User;
-  userId: string;
-  record: Record[];
-};
+// プロフィールUserの定義
+export interface User {
+  name: string;
+  email: string;
+  studentProfile: StudentProfile | null;
+}
 
-export type Record = {
-  id: string;
-  content: string | null;
-  progress: string | null;
-  date: Date;
-  studentProfileId: string;
-  studentProfile: StudentProfile;
-};
-
-export type Account = {
-  id: string;
-  userId: string;
-  type: string;
-  provider: string;
-  providerAccountId: string;
-  refresh_token: string | null;
-  access_token: string | null;
-  expires_at: number | null;
-  token_type: string | null;
-  scope: string | null;
-  id_token: string | null;
-  session_state: string | null;
-  user: User;
-};
-
-export type StaffNg = {
-  id: string;
-  email: string | null;
-  ymd: string | null;
-  time: string[];
-  staffName: string | null;
-};
-
-export type WaitingList = {
-  id: string;
-  studentEmail: string | null;
-  staffEmail: string | null;
-  ymd: string | null;
-  time: string[];
-  details: string | null;
-};
-
-export type Booking = {
-  id: string;
-  studentEmail: string | null;
-  staffEmail: string | null;
-  ymd: string | null;
-  time: string[];
-  details: string | null;
-};
+// プロフィールApiResponseの定義
+export interface ApiResponse {
+  message: string;
+  user: User[];
+}
+//-----------------------------------------------------------------------------------------
