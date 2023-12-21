58831d5524554ceb2be80d31a938e3a7d132c40a
Aruche-code <mo.crew2435@gmail.com>
2023-12-21 12:35:18 +0900

    卒業年度修正

--- app/(root)/student/profile/page.tsx
+++ app/(root)/student/profile/page.tsx
@@ -28,7 +28,7 @@ function UserForm() {
   const currentYear = new Date().getFullYear();
   const graduationYears = Array.from(
     { length: 4 },
-    (_, i) => currentYear + i
+    (_, i) => currentYear + i + 1
   ).map(String);
 
   useEffect(() => {
@@ -94,19 +94,22 @@ function UserForm() {
   return (
     <div className="flex flex-col flex-1 p-1.5 items-center">
       <div className="bg-gray-100 shadow-lg flex flex-col items-center w-full md:w-4/5 lg:w-2/3 xl:w-1/2 text-xs md:text-sm lg:text-sm rounded-lg">
-
-        <h1 className="mt-8 mb-2 text-xl text-gray-800 font-semibold">プロフィール</h1>
+        <h1 className="mt-8 mb-2 text-xl text-gray-800 font-semibold">
+          プロフィール
+        </h1>
 
         <form onSubmit={handleSubmit} className="w-full mx-auto">
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 py-1 mx-auto">
             <label className="text-gray-900 w-1/3">名前</label> {userData?.name}
           </div>
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 py-1 mx-auto">
-            <label className="text-gray-900 w-1/3">メールアドレス</label> {userData?.email}
+            <label className="text-gray-900 w-1/3">メールアドレス</label>{" "}
+            {userData?.email}
           </div>
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 mx-auto">
             <label className=" text-gray-900 w-1/3">電話番号</label>
-            <input className={`p-1 border-1 rounded-lg w-2/3 px-4`}
+            <input
+              className={`p-1 border-1 rounded-lg w-2/3 px-4`}
               type="text"
               name="tel"
               value={profileData.tel}
@@ -116,7 +119,8 @@ function UserForm() {
           {/* 各フォームフィールド */}
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 mx-auto">
             <label className="text-gray-900 w-1/3">学科</label>
-            <input className="p-1 border-1 rounded-lg w-2/3 px-4"
+            <input
+              className="p-1 border-1 rounded-lg w-2/3 px-4"
               type="text"
               name="department"
               value={profileData.department}
@@ -125,7 +129,8 @@ function UserForm() {
           </div>
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 mx-auto">
             <label className=" text-gray-900 w-1/3">学年</label>
-            <select className="p-1 border-1 rounded-lg w-1/3 md:w-1/4 px-4"
+            <select
+              className="p-1 border-1 rounded-lg w-1/3 md:w-1/4 px-4"
               name="schoolYear"
               value={profileData.schoolYear}
               onChange={handleChange}
@@ -140,7 +145,8 @@ function UserForm() {
           </div>
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 mx-auto">
             <label className=" text-gray-900 w-1/3">卒業予定</label>
-            <select className="p-1 border-1 rounded-lg w-1/3 md:w-1/4 px-4"
+            <select
+              className="p-1 border-1 rounded-lg w-1/3 md:w-1/4 px-4"
               name="graduationYear"
               value={profileData.graduationYear}
               onChange={handleChange}
@@ -156,7 +162,8 @@ function UserForm() {
 
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 mx-auto">
             <label className=" text-gray-900 w-1/3">希望勤務地</label>
-            <input className="p-1 border-1 rounded-lg w-2/3 px-4"
+            <input
+              className="p-1 border-1 rounded-lg w-2/3 px-4"
               type="text"
               name="workLocation"
               value={profileData.workLocation}
@@ -165,7 +172,8 @@ function UserForm() {
           </div>
           <div className="flex items-center mt-3 w-3/4 sm:w-8/12 mx-auto">
             <label className=" text-gray-900 w-1/3">資格</label>
-            <textarea className="p-1 border-1 rounded-lg w-2/3 px-4"
+            <textarea
+              className="p-1 border-1 rounded-lg w-2/3 px-4"
               name="qualification"
               value={profileData.qualification}
               onChange={handleChange}
@@ -174,12 +182,19 @@ function UserForm() {
             />
           </div>
 
-          <button className="flex items-center px-20 py-2 text-center text-white bg-blue-600 rounded-xl hover:bg-blue-700 mt-7 mb-7 mx-auto"
-            type="submit">{isEditing ? "保存" : "新規作成"}</button>
+          <button
+            className="flex items-center px-20 py-2 text-center text-white bg-blue-600 rounded-xl hover:bg-blue-700 mt-7 mb-7 mx-auto"
+            type="submit"
+          >
+            {isEditing ? "保存" : "新規作成"}
+          </button>
           {/* {isEditing && (
-            <button className="flex items-center px-20 py-2 text-center text-white bg-red-400 rounded-xl hover:bg-red-500 mb-7 mx-auto"
-              type="button" onClick={handleDelete}>
-              入力リセット
+            <button
+              className="flex items-center px-20 py-2 text-center text-white bg-red-400 rounded-xl hover:bg-red-500 mb-7 mx-auto"
+              type="button"
+              onClick={handleDelete}
+            >
+              プロフィール情報を削除
             </button>
           )} */}
         </form>
