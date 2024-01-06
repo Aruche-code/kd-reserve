cd73b41306be96de4010f858bf5b348095cbc7d8
Aruche-code <mo.crew2435@gmail.com>
2024-01-07 01:24:35 +0900

    予約画面で使用するコンポーネント

--- app/components/posts/BookingPost.tsx
+++ app/components/posts/BookingPost.tsx
@@ -82,9 +82,18 @@ const BookingPost: React.FC<BookingPostProps> = ({
 
   return (
     <>
-      <button onClick={handleWaitingSubmit} disabled={isLoading}>
-        {isLoading ? "送信中..." : "Submit"}
+      <button
+        onClick={handleWaitingSubmit}
+        disabled={isLoading}
+        className={`px-6 py-2 border rounded text-white font-bold transition duration-300 ease-in-out transform ${
+          isLoading
+            ? "bg-gray-400 cursor-not-allowed"
+            : "bg-kd-button-cl hover:bg-blue-500 hover:scale-110"
+        }`}
+      >
+        {isLoading ? "送信中..." : "送信"}
       </button>
+
       {/* <p>{message}</p> メッセージの表示 */}
     </>
   );
