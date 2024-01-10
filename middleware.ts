bd08ff6876200e0b5f2b2ab69309a0f9de2a13e4
ShunsakuKatsumata <kd1310700@st.kobedenshi.ac.jp>
2024-01-10 22:06:00 +0900

    punipuni

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,19 @@ export default withAuth({
     signIn: "/",
   },
   callbacks: {
-    authorized: ({ req, token }) => {
-    // "/staff" へのアクセスは role が "staff" の場合のみ許可
-    if (req.nextUrl.pathname.startsWith("/staff")) {
-    return token?.role === "staff";
-    }
-    // "/student" へのアクセスは role が "student" の場合のみ許可
-    else if (req.nextUrl.pathname.startsWith("/student")) {
-    return token?.role === "student";
-    }
 
-    // 上記以外のパスへのアクセスは全て拒否
-    return false;
+    authorized: ({ req, token }) => {
+      // "/staff" へのアクセスは role が "staff" の場合のみ許可
+      if (req.nextUrl.pathname.startsWith("/staff")) {
+        return token?.role === "staff";
+      }
+      // "/student" へのアクセスは role が "student" の場合のみ許可
+      else if (req.nextUrl.pathname.startsWith("/student")) {
+        return token?.role === "student";
+      }
+
+      // 上記以外のパスへのアクセスは全て拒否
+      return false;
     },
   },
 });
