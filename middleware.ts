5319c898331c80dd566b5fc378d357a96b647ff6
Aruche-code <mo.crew2435@gmail.com>
2024-01-10 22:14:35 +0900

    修正

--- middleware.ts
+++ middleware.ts
@@ -6,19 +6,20 @@ export default withAuth({
   },
   // callbacks: {
   //   authorized: ({ req, token }) => {
-  //   // "/staff" へのアクセスは role が "staff" の場合のみ許可
-  //   if (req.nextUrl.pathname.startsWith("/staff")) {
-  //   return token?.role === "staff";
-  //   }
-  //   // "/student" へのアクセスは role が "student" の場合のみ許可
-  //   else if (req.nextUrl.pathname.startsWith("/student")) {
-  //   return token?.role === "student";
-  //   }
+  //     // "/staff" へのアクセスは role が "staff" の場合のみ許可
+  //     if (req.nextUrl.pathname.startsWith("/staff")) {
+  //       return token?.role === "staff";
+  //     }
+  //     // "/student" へのアクセスは role が "student" の場合のみ許可
+  //     else if (req.nextUrl.pathname.startsWith("/student")) {
+  //       return token?.role === "student";
+  //     }
 
-  //   // 上記以外のパスへのアクセスは全て拒否
-  //   return false;
+  //     // 上記以外のパスへのアクセスは全て拒否
+  //     return false;
   //   },
   // },
+});
 
 export const config = {
   matcher: ["/student/:path*", "/staff/:path*"],
