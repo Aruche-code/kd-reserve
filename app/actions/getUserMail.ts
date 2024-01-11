bfdade11a036fcf6bf774dc0c87eba48037ed0a2
Aruche-code <mo.crew2435@gmail.com>
2024-01-11 10:17:08 +0900

    作り直し

---
+++ app/actions/getUserMail.ts
@@ -0,0 +1,20 @@
+import getSession from "./getSession";
+
+// 使い方 const usermail = await getUserMail();
+
+const getUserMail = async () => {
+  try {
+    const session = await getSession();
+
+    // セッション情報が存在しない、またはセッションにユーザーのメールアドレスが含まれていない場合、nullを返す
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    return session.user.email;
+  } catch (error) {
+    return null;
+  }
+};
+
+export default getUserMail;
