9ff34c6773ae45ae524a032a729dd37ef14efbfb
Aruche-code <mo.crew2435@gmail.com>
2024-01-16 13:22:45 +0900

    newPrismaを毎回しない様にインポートを変更

--- app/api/staff/calendar/route.ts
+++ app/api/staff/calendar/route.ts
@@ -1,140 +1,153 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-import getUserMail from "@/app/actions/getUsermail";
-
-
-const prisma = new PrismaClient();
+import prisma from "@/app/libs/prismadb";
+import getUserMail from "@/app/actions/getUserMail";
 
 // DB接続関数
 export async function main() {
-    try {
-        await prisma.$connect();
-    } catch (err) {
-        return Error("DB接続に失敗しました");
-    }
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("DB接続に失敗しました");
+  }
 }
 
 // NG日程の作成
 export const POST = async (req: Request, res: NextResponse) => {
-    console.log("create");
-    try {
-        //const staffEmail = await getStaffUsers();    // 教員セッション情報を取得
-        const email = await getUserMail();   // 変数emailにセッション情報から取得したemail情報を格納する
-        //const email = "yama@master.mail.com" // emailを格納
-        await main();
-        const { ymd ,time} = await req.json();
+  console.log("create");
+  try {
+    //const staffEmail = await getStaffUsers();    // 教員セッション情報を取得
+    const email = await getUserMail(); // 変数emailにセッション情報から取得したemail情報を格納する
+    //const email = "yama@master.mail.com" // emailを格納
+    await main();
+    const { ymd, time } = await req.json();
 
-        const user = await prisma.user.findUnique({
-            where: { email },
-        });
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-        const postedStaffNg = await prisma.staffNg.create({ // emailと一致するuserにstaffNgを作成
-            data: {
-                ymd,
-                time,
-                user: {
-                    connect: { id: user?.id },
-                },
-            },
-        });
+    const postedStaffNg = await prisma.staffNg.create({
+      // emailと一致するuserにstaffNgを作成
+      data: {
+        ymd,
+        time,
+        user: {
+          connect: { id: user?.id },
+        },
+      },
+    });
 
-        return NextResponse.json({ message: "Success", /* postedStaffNg */ }, { status: 201 });
-    } catch (err) {
-        return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-        await prisma.$disconnect();
-    }
+    return NextResponse.json(
+      { message: "Success" /* postedStaffNg */ },
+      { status: 201 }
+    );
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 // NG日程と予約確定日時の表示
 export const GET = async (req: Request, res: NextResponse) => {
-    console.log("GET");
+  console.log("GET");
 
-    try {
-        // const email = await getUsermail()  // 変数emailにセッション情報から取得したemail情報を格納する
-        const email = "yama@master.mail.com" // emailを格納
-        await main();
-        const getedStaffNgBooking = await prisma.user.findUnique({ // emailと一致するuserのid、名前、emailとstaffNgテーブル、bookingテーブルを表示
-            where: { email },
-            include: {
-                staffng: true,
-                booking: true,
-            },
-        });
+  try {
+    // const email = await getUsermail()  // 変数emailにセッション情報から取得したemail情報を格納する
+    const email = "yama@master.mail.com"; // emailを格納
+    await main();
+    const getedStaffNgBooking = await prisma.user.findUnique({
+      // emailと一致するuserのid、名前、emailとstaffNgテーブル、bookingテーブルを表示
+      where: { email },
+      include: {
+        staffng: true,
+        booking: true,
+      },
+    });
 
-        const responseData = getedStaffNgBooking  // responseの編集
-            ? {
-                id: getedStaffNgBooking.id,
-                name: getedStaffNgBooking.name,
-                email: getedStaffNgBooking.email,
-                staffng: getedStaffNgBooking.staffng
-                    ? getedStaffNgBooking.staffng.map((staffng) => ({
-                        ymd: staffng?.ymd,
-                        time: staffng?.time,
-                    }))
-                    : null,
-                booking: getedStaffNgBooking.booking
-                    ? getedStaffNgBooking.booking.map((booking) => ({
-                        studentEmail: booking?.studentEmail,
-                        staffEmail: booking?.staffEmail,
-                        ymd: booking?.ymd,
-                        time: booking?.time,
-                        details: booking?.details,
-                    }))
-                    : null,
-            }
-            : null;
+    const responseData = getedStaffNgBooking // responseの編集
+      ? {
+          id: getedStaffNgBooking.id,
+          name: getedStaffNgBooking.name,
+          email: getedStaffNgBooking.email,
+          staffng: getedStaffNgBooking.staffng
+            ? getedStaffNgBooking.staffng.map((staffng) => ({
+                ymd: staffng?.ymd,
+                time: staffng?.time,
+              }))
+            : null,
+          booking: getedStaffNgBooking.booking
+            ? getedStaffNgBooking.booking.map((booking) => ({
+                studentEmail: booking?.studentEmail,
+                staffEmail: booking?.staffEmail,
+                ymd: booking?.ymd,
+                time: booking?.time,
+                details: booking?.details,
+              }))
+            : null,
+        }
+      : null;
 
-        return NextResponse.json({ message: "Success", responseData }, { status: 200 });
-    } catch (err) {
-        return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-        await prisma.$disconnect();
-    }
+    return NextResponse.json(
+      { message: "Success", responseData },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 // 指定したidのNG日程を編集
 export const PUT = async (req: Request, res: NextResponse) => {
-    console.log("PUT");
+  console.log("PUT");
 
-    try {
-        const staffNgId = "658eedaad7973a3b99ca5db0" // staffNgIdに職員のオブジェクトidを格納する
-        await main();
-        const { ymd, time } = await req.json();
+  try {
+    const staffNgId = "658eedaad7973a3b99ca5db0"; // staffNgIdに職員のオブジェクトidを格納する
+    await main();
+    const { ymd, time } = await req.json();
 
-        const updatedStaffNg = await prisma.staffNg.update({ // staffNgIdと一致するstaffNgテーブルを編集
-            where: { id: staffNgId },
-            data: {
-                ymd,
-                time,
-            },
-        });
+    const updatedStaffNg = await prisma.staffNg.update({
+      // staffNgIdと一致するstaffNgテーブルを編集
+      where: { id: staffNgId },
+      data: {
+        ymd,
+        time,
+      },
+    });
 
-        return NextResponse.json({ message: "Success", /* updatedStaffNg */ }, { status: 200 });
-    } catch (err) {
-        return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-        await prisma.$disconnect();
-    }
+    return NextResponse.json(
+      { message: "Success" /* updatedStaffNg */ },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 // 指定したidのNG日程を削除
 export const DELETE = async (req: Request, res: NextResponse) => {
-    console.log("DELETE");
+  console.log("DELETE");
 
-    try {
-        const staffNgId = "658eedaad7973a3b99ca5db0" // staffNgIdに職員のオブジェクトidを格納する
-        await main();
-        const deletedStaffNg = await prisma.staffNg.delete({ // staffNgIdと一致するstaffNgテーブルを削除
-            where: {
-                id: staffNgId
-            },
-        });
+  try {
+    const staffNgId = "658eedaad7973a3b99ca5db0"; // staffNgIdに職員のオブジェクトidを格納する
+    await main();
+    const deletedStaffNg = await prisma.staffNg.delete({
+      // staffNgIdと一致するstaffNgテーブルを削除
+      where: {
+        id: staffNgId,
+      },
+    });
 
-        return NextResponse.json({ message: "Success", /* deletedStaffNg */ }, { status: 200 });
-    } catch (err) {
-        return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-        await prisma.$disconnect();
-    }
+    return NextResponse.json(
+      { message: "Success" /* deletedStaffNg */ },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
 };
