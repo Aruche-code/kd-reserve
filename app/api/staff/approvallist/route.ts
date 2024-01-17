24e1f1af120389a4ebac067861f01e2c116469fa
hiroki04y <hiroki02y104@icloud.com>
2024-01-17 09:29:13 +0900

    approvallistテスト用email削除、本番用処理追加

--- app/api/staff/approvallist/route.ts
+++ app/api/staff/approvallist/route.ts
@@ -116,8 +116,8 @@ export const GET = async (req: Request, res: NextResponse) => {
   console.log("GET");
 
   try {
-    // const email = await getUsermail() 本番
-    const email = "sample3@gmail.com"; //テスト
+    const email = await getUsermail() 本番
+    //const email = "sample3@gmail.com"; //テスト
     await main();
     const wait = await prisma.user.findMany({
       where: { email },
