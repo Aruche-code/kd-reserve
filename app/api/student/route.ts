2f739c7d8d033678b0291395af6592849326583b
st0H034 <kd1307152@st.kobedenshi.ac.jp>
2024-01-17 14:10:00 +0900

    add: api/studentにDELETE機能を追加

--- app/api/student/route.ts
+++ app/api/student/route.ts
@@ -70,3 +70,23 @@ export const GET = async (req: Request, res: NextResponse) => {
     await prisma.$disconnect(); // DBへの接続を閉じる
   }
 };
+
+
+// DELETE
+// 指定された予定を削除するAPI
+export const DELETE = async (req: Request, res: NextResponse) => {
+
+  try {
+      const { scheduleId, } = await req.json();
+
+      await main();
+      const user = await prisma.waitingList.delete({
+          where: { id: scheduleId },
+      });
+      return NextResponse.json({ message: "Success", user }, { status: 200 });
+  } catch (err) {
+      return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+      await prisma.$disconnect();
+  }
+};
