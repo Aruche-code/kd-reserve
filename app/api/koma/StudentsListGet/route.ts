543dc9514cfffe522d3b5cf1b672607903764589
Komakawa <kd1333983@st.kobedenshi.ac.jp>
2024-01-17 11:45:47 +0900

    学生一覧取得API(一覧取得と検索を分割)

--- app/api/koma/StudentsListGet/route.ts
+++ app/api/koma/StudentsListGet/route.ts
@@ -1,5 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next";   // APIルートのリクエストとレスポンスの型定義
-
 import { NextResponse } from "next/server";               // APIミドルウェアでレスポンスを操作するためのオブジェクト
 import { PrismaClient } from "@prisma/client";            // データベース接続とクエリのためのメインオブジェクト
 import getStaffUsers from "@/app/actions/getStaffUsers";  //教員のセッション情報認証ロジックをインポート
@@ -17,57 +15,33 @@ export async function main() {
   }
 }
 
-
 // 学生一覧を取得するAPI
-// export const GET = async (req: Request, res: NextResponse) => {
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("GET Students");
-  try {
+export const GET = async (req: Request, res: NextResponse) => {
+  console.log("GET All Students");
+  try{
     await main(); // DB接続関数の呼び出し
-    // const { searchValue } = req.query;  // 検索値をリクエストから取得
-    const searchValue = "133398";      // 仮の検索値入力
-    // // const searchValue = req.query?.searchValue;
+    const user = await prisma.user.findMany({
+      where: { role: "student" },  // 学生のみを検索対象とする
+      select: {
+        name: true,
+        email: true,
+      },
+    })
 
-    if (searchValue) {
-      const user = await prisma.user.findMany({
-        where: {
-          role: "student",  // 学生のみを検索対象とする
-          email: {          // 学籍番号が紐づけられたmailAddressを取得
-            endsWith: `@st.kobedenshi.ac.jp`,
-            contains: `kd${searchValue}`,
-          },
-        },
-        select: {           // 表示するフィールドを指定する
-          name: true,
-          email: true,
-          // studentProfile: true, // プロフィール
-          // record: true,         // カルテ情報
-          // booking: true,        // 面談予約情報
-        },
-      });
+    // emailから数値の部分(学籍番号)のみを取り出す部品
+    const transformedUsers = user.map(user => ({
+      numericPartOfEmail:
+        user.email ?                          // nullチェックを行い、以下のreplaceメソッドを安全に呼び出す
+        user.email.replace(/\D/g, '') : "",   // \D: 数字以外を表す正規表現
+      name: user.name,
+      email: user.email,
+    }));
 
-      // emailから数値の部分のみを取り出す
-      const transformedUsers = user.map(user => ({
-        name: user.name,
-        numericPartOfEmail: user.email.replace(/\D/g, ''), // \D: 数字以外を表す正規表現
-      }));
-
-      if (transformedUsers.length > 0) {
-        return NextResponse.json(
-          { message: "Success",
-            searchValue,              // 検索値(学籍番号)を返却
-            // user,                     // 学生情報(name, email)
-            users: transformedUsers,  // 学生情報(name, 学籍番号)
-          },
-          { status: 200 }
-        );
-      } else {
-        return NextResponse.json(
-          { message: "一致する学生は見つかりませんでした" },
-          { status: 404 }
-        );
-      }
-    }
+    // 成功時のレスポンス
+    return NextResponse.json(
+      { message: "Success", users: transformedUsers, },
+      { status: 200 }           // ステータスコード all OK
+    );
   } catch (err) {
     console.error("Error:", err);   // エラー時の確認用ログ出力
     return NextResponse.json({ message: "Error", err }, { status: 500 });
