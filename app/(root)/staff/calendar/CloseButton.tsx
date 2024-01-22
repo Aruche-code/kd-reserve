1a4599fc3f44397f164c70ef2c859e86fbb399ca
hiroki04y <hiroki02y104@icloud.com>
2024-01-22 11:27:53 +0900

    closeボタンエラー対応

--- app/(root)/staff/calendar/CloseButton.tsx
+++ app/(root)/staff/calendar/CloseButton.tsx
@@ -1,3 +1,4 @@
+import CloseIcon from '@mui/icons-material/Close';
 export default function CloseButton({
     setShowModal,
     setNgModal,
@@ -10,20 +11,7 @@ export default function CloseButton({
                 onClick={() => {setShowModal(false),setNgModal(false),setInterModal(false),setOpen(false)}} 
                 className="absolute top-3 right-3"
             >
-                <svg 
-                className="w-6 h-6 text-4xl" 
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg" 
-                >
-                <path  
-                    strokeLinecap="round"
-                    strokeLinejoin="round"  
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                />
-                </svg>
+                <CloseIcon className="w-6 h-6 text-4xl" />
             </button>
         </>
     )
