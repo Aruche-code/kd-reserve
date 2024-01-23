dbf5d9ab71ecefa5ff503bbe38d9fc6b3e0a50d6
hiroki04y <hiroki02y104@icloud.com>
2024-01-23 14:11:52 +0900

    staffhome

--- app/(root)/staff/page.tsx
+++ app/(root)/staff/page.tsx
@@ -4,89 +4,103 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Link from "next/link";
+import useSWR from "swr";
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+interface Booking {
+  id: string;
+  // その他の予約関連のフィールド
+}
+
+interface HomeData {
+  message: string;
+  getBookingList: Booking[];
+}
 
 const Home = () => {
-  const [homeData, setHomeData] = useState<any>({ message: '', getBookingList: [] });
-  const [isLoading, setIsLoading] = useState(true);
+  const {
+    data: homeData,
+    error,
+    mutate,
+  } = useSWR<HomeData>("/api/staff", fetcher);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/api/staff'); // 仮のAPIエンドポイント
-        setHomeData(response.data);
-      } catch (error) {
-        console.error('Error fetching home data:', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    console.log(homeData)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [homeData]);
 
-    fetchData();
-  }, []);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  if (error) return <div>エラーが発生しました</div>;
 
-  // const Staff = () => {
-  //   const testUsers = [
-  //     {
-  //       id: '601b92ee95861639c3e2c44b',
-  //       kana: 'コウベタロウ',
-  //       name: '神戸太郎',
-  //       gakuseki: '1111111',
-  //       day: '2023/12/3',
-  //       time: '10:00~12:00',
-  //       subject: '面接練習'
-  //     },
-  //     {
-  //       id: '601b95a595861639c3e2c44c',
-  //       kana: 'コウベジロウ',
-  //       name: '神戸次郎',
-  //       gakuseki: '2222222',
-  //       day: '2023/12/3',
-  //       time: '13:00~14:00',
-  //       subject: 'エントリーシート作成'
-  //     },
-  //   ];
+
+  const SkeletonRow = () => (
+    <div className="mb-3 grid grid-cols-1 sm:grid-cols-4 gap-4 bg-gray-100 rounded-lg shadow-sm p-4 items-center animate-pulse">
+      {[...Array(4)].map((_, index) => (
+        <div key={index} className="h-8 bg-gray-300 rounded"></div>
+      ))}
+    </div>
+  );
 
   return (
 
-    <div className="flex flex-row items-center justify-center">
-      <div className="flex flex-col mt-10 w-full lg:w-1/2  bg-white rounded-md shadow-md">
+    <div className="flex flex-col justify-center items-center">
+      <div className="mt-4 w-full lg:w-4/5 bg-white rounded-md shadow-md">
 
-        <div className="p-3 px-6 rounded-t-lg bg-kd-sub2-cl text-white">
+        <div className="p-3 px-6 rounded-t-lg bg-kd-s text-white">
           ■ 本日の予定
         </div>
         {/* <div className="mt-1 mb-1 flex-grow border-b border-gray-400" ></div> */}
-        <div className="mt-5 mb-5">
-          {homeData.getBookingList.length === 0 ? (
+        <div className="p-4">
+        {homeData?.getBookingList ? (
+          homeData?.getBookingList.length === 0 ? (
             <div className="text-center">本日の予定はありません</div>
           ) : (
-            homeData.getBookingList.map((booking: any) => (
-              <div className="flex flex-col p-2" key={booking.id}>
-
-                <Link href="../staff/record">
-                  <div className="mx-4 p-2 border-2 border-gray-200 bg-white rounded-lg flex flex-row">
-
-                    <div className="w-full flex flex-row text-center items-center justify-center text-xs md:text-base lg:text-sm xl:text-base">
-
-                      <div className="w-1/3 px-3">
-                        {booking.studentName}
-                      </div>
-                      <div className="w-1/3 px-3">
-                        {booking.ymd}<br />
-                        {booking.time}
-                      </div>
-                      <div className="w-1/3 px-3">
-                        {booking.details}
-                      </div>
-                    </div>
+            homeData?.getBookingList.map((booking: any) => (
+              <div
+                  className="mb-3 grid grid-cols-1 sm:grid-cols-4 gap-4 bg-gray-100 rounded-lg shadow-sm p-4 items-center"
+                  key={booking.staffName}
+                >
+                  {/* 生徒名 */}
+                  <div className="flex flex-col">
+                    <span className="font-bold text-xs md:text-sm mb-2">
+                      生徒名
+                    </span>
+                    <span className="text-sm md:text-base">{booking.studentName}</span>
                   </div>
 
+                  {/* 日付 */}
+                  <div className="flex flex-col">
+                    <span className="font-bold text-xs md:text-sm mb-2">
+                      日付
+                    </span>
+                    <span className="text-sm md:text-base">{booking.ymd}</span>
+                  </div>
+
+                  {/* 時間帯 */}
+                  <div className="flex flex-col">
+                    <span className="font-bold text-xs md:text-sm mb-2">
+                      時間帯
+                    </span>
+                    <span className="text-sm md:text-base">{booking.time}</span>
+                  </div>
+
+                  {/* 詳細 */}
+                  <div className="flex flex-col">
+                    <span className="font-bold text-xs md:text-sm mb-2">
+                      詳細
+                    </span>
+                    <span className="text-sm md:text-base">
+                      {booking.details}
+                    </span>
+                  </div>
+                  <Link href="../staff/record">
                 </Link>
               </div>
-            )))}
+            ))
+          )
+        ) :(
+          <SkeletonRow />
+        )}
         </div>
       </div>
 
