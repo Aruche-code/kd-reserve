847732cdf7e173f7e689edbb846ddf1e6d42b87b
Aruche-code <mo.crew2435@gmail.com>
2024-01-24 05:57:42 +0900

    成形しました

--- app/(root)/components/BookingPost.tsx
+++ app/(root)/components/BookingPost.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import axios from "axios";
 import { format } from "date-fns";
 import { toast } from "react-hot-toast";
+import PersonIcon from "@mui/icons-material/Person";
+import InfoIcon from "@mui/icons-material/Info";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 /**
  * StaffList Component
@@ -174,35 +177,75 @@ const BookingPost: React.FC<BookingPostProps> = ({
 
   // モーダルのレンダリング
   const renderModal = () => (
-    <div className="modal-backdrop">
-      <div className="modal">
-        <div className="p-3 px-6 rounded-t-md bg-kd-s text-white text-center">
+    <div className="fixed inset-0 bg-black bg-opacity-10 flex justify-center items-center z-50">
+      <div className="bg-kd-m p-6 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-lg text-kd-a_100 font-semibold mb-4 ">
           予約内容を確認
+        </h2>
+        <div className="grid grid-cols-2 gap-4">
+          <p className="col-span-2">
+            <span className="font-medium">
+              <span className="flex items-center">
+                <InfoIcon style={{ marginRight: 5, opacity: 0.4 }} />
+                {selectedTag}
+              </span>
+            </span>
+          </p>
+          <p className="col-span-2">
+            <span className="font-medium">
+              <span className="flex items-center">
+                <PersonIcon style={{ marginRight: 5, opacity: 0.4 }} />
+                {findStaffNameById(staffData, selectedStaffMember)}
+              </span>
+            </span>
+          </p>
+          <p className="col-span-2 mt-3">
+            <span className="  font-bold text-kd-black-cl">
+              希望日１:{"  "}
+            </span>
+            <span className="font-medium">
+              {firstPreferenceDate
+                ? format(firstPreferenceDate, "yyyy-MM-dd")
+                : null}
+              <span className="flex items-center mt-1">
+                <AccessTimeIcon style={{ marginRight: 5, opacity: 0.4 }} />
+                {firstPreferenceStartTime?.value}～
+                {firstPreferenceEndTime?.value}
+              </span>
+            </span>
+          </p>
+          <p className="col-span-2 mt-3">
+            <span className="  font-bold text-kd-black-cl">
+              希望日２:{"  "}
+            </span>
+            <span className="font-medium">
+              {secondPreferenceDate
+                ? format(secondPreferenceDate, "yyyy-MM-dd")
+                : null}
+              <span className="flex items-center mt-1">
+                <AccessTimeIcon style={{ marginRight: 5, opacity: 0.4 }} />
+                {secondPreferenceStartTime?.value}～
+                {secondPreferenceEndTime?.value}
+              </span>
+            </span>
+          </p>
+          <p className="col-span-2 mt-3">
+            <span className="  font-bold text-kd-black-cl">
+              希望日３:{"  "}
+            </span>
+            <span className="font-medium">
+              {thirdPreferenceDate
+                ? format(thirdPreferenceDate, "yyyy-MM-dd")
+                : null}
+              <span className="flex items-center">
+                <AccessTimeIcon style={{ marginRight: 5, opacity: 0.4 }} />
+                {thirdPreferenceStartTime?.value}～
+                {thirdPreferenceEndTime?.value}
+              </span>
+            </span>
+          </p>
         </div>
-        <div>
-          <div>{selectedTag}</div>
-          <div>{findStaffNameById(staffData, selectedStaffMember)}</div>
-          <div>
-            {firstPreferenceDate
-              ? format(firstPreferenceDate, "yyyy-MM-dd")
-              : null}
-          </div>
-          <div>{firstPreferenceStartTime?.value}</div>
-          <div>{firstPreferenceEndTime?.value}</div>
-          <div>
-            {secondPreferenceDate
-              ? format(secondPreferenceDate, "yyyy-MM-dd")
-              : null}
-          </div>
-          <div>{secondPreferenceStartTime?.value}</div>
-          <div>{secondPreferenceEndTime?.value}</div>
-          <div>
-            {thirdPreferenceDate
-              ? format(thirdPreferenceDate, "yyyy-MM-dd")
-              : null}
-          </div>
-          <div>{thirdPreferenceStartTime?.value}</div>
-          <div>{thirdPreferenceEndTime?.value}</div>
+        <div className="flex justify-between mt-4">
           <button
             onClick={handleConfirmSubmit}
             disabled={isLoading}
@@ -211,32 +254,21 @@ const BookingPost: React.FC<BookingPostProps> = ({
                 ? "bg-gray-400 cursor-not-allowed"
                 : "bg-kd-a_100 hover:bg-blue-400 hover:scale-105"
             }`}
-            style={{ minWidth: "100px" }} // 最小幅を設定
           >
-            <div
-              className="flex justify-center items-center"
-              style={{ minWidth: "80px" }}
-            >
-              {isLoading ? "送信中..." : "送信"}
-            </div>
+            {isLoading ? "送信中..." : "送信"}
           </button>
           <button
             onClick={closeModal}
-            className="px-4 py-1.5 text-sm text-white rounded transition duration-300 ease-in-out transform bg-red-500 hover:bg-red-600 hover:scale-105 "
-            style={{ minWidth: "100px" }} // 最小幅を設定
+            className="px-4 py-2 text-sm text-white rounded transition duration-300 ease-in-out transform bg-red-500 hover:bg-red-600 hover:scale-105"
           >
-            <div
-              className="flex justify-center items-center"
-              style={{ minWidth: "80px" }}
-            >
-              キャンセル
-            </div>
+            戻る
           </button>
         </div>
       </div>
     </div>
   );
 
+  // text-sm text-white rounded transition duration-300 ease-in-out transform bg-red-500 hover:bg-red-600 hover:scale-105
   return (
     <>
       <button
