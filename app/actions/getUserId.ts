e33583e65bef977929035252952e302267d15a38
Aruche-code <mo.crew2435@gmail.com>
2024-01-26 12:28:05 +0900

    戻しました

--- app/actions/getUserId.ts
+++ app/actions/getUserId.ts
@@ -6,18 +6,15 @@ import prisma from "@/app/libs/prismadb";
 
 const getUserId = async (email: string) => {
   try {
-    if (!email) {
-      return null;
-    } else {
-      const UserData: any = await prisma.user.findUnique({
-        where: { email: email },
-        select: {
-          id: true, // idを取得
-        },
-      });
+    // 画面を操作しているユーザーのidを取得
+    const UserData: any = await prisma.user.findUnique({
+      where: { email: email },
+      select: {
+        id: true, // idを取得
+      },
+    });
 
-      return UserData.id;
-    }
+    return UserData.id;
   } catch (error) {
     return null; // 引数で受け取ったemailのユーザは存在しません。
   }
