83d461eb0d9da958c1b5ee1f210a49adf0d4ec1b
Aruche-code <mo.crew2435@gmail.com>
2024-01-26 12:15:59 +0900

    魔法のif文

--- app/actions/getUserId.ts
+++ app/actions/getUserId.ts
@@ -6,14 +6,18 @@ import prisma from "@/app/libs/prismadb";
 
 const getUserId = async (email: string) => {
   try {
-    const UserData: any = await prisma.user.findUnique({
-      where: { email: email },
-      select: {
-        id: true, // idを取得
-      },
-    });
+    if (!email) {
+      return null;
+    } else {
+      const UserData: any = await prisma.user.findUnique({
+        where: { email: email },
+        select: {
+          id: true, // idを取得
+        },
+      });
 
-    return UserData.id;
+      return UserData.id;
+    }
   } catch (error) {
     return null; // 引数で受け取ったemailのユーザは存在しません。
   }
