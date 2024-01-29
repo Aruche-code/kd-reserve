ff94093b4832fe4659ee1f3016a79dd216a92494
Kanon74 <kd1320455@st.kobedenshi.ac.jp>
2024-01-29 13:26:24 +0900

    staff/students

--- app/(root)/staff/students/page.tsx
+++ app/(root)/staff/students/page.tsx
@@ -48,48 +48,71 @@ const Students = () => {
     setSearchTerm(event.target.value);
   };
 
+  const SkeletonRow = () => (
+    <div className="m-5 mt-0 py-2 flex flex-wrap justify-center items-center">
+      {Array.from({ length: 3 }, (_, index) => (
+        <div
+          key={index}
+          className={"flex flex-col my-2 mx-5 min-w-[24ch] px-8 p-5 py-4 border-2 bg-white border-gray-100 shadow-md rounded-lg hover:border-2 hover:border-kd-sub2-cl overflow-hidden animate-pulse"}
+        >
+          <div className="mb-1 px-2 py-3 bg-gray-300 rounded animate-pulse"></div>
+          <div className="px-2 py-3 bg-gray-300 rounded animate-pulse"></div>
+        </div>
+      ))}
+    </div>
+
+  );
+
   return (
     <div className="flex flex-col justify-center items-center mt-6">
-      <div className="bg-gray-100 rounded-lg shadow-md w-4/5 mt-5">
+      <div className="bg-white rounded-lg shadow-md w-4/5 mt-5">
         <div className="p-3 px-6 rounded-t-lg bg-kd-sub2-cl text-white">
           ■ 学生一覧
         </div>
-        <div className="mt-3 p-3">
-          <div className="relative flex justify-end">
-            <div className="flex justify-center w-full px-4">
-              <div className="relative w-full max-w-sm">
-                <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
-                <input
-                  className="pl-10 pr-3 py-2 border rounded-full w-full hover:shadow-lg focus:shadow-lg focus:outline-0"
-                  type="text"
-                  placeholder="検索"
-                  value={searchTerm}
-                  onChange={handleSearchChange}
-                />
+        <div className="bg-gray-100 m-5 rounded">
+          <div className="mt-3 p-3 pt-5">
+            <div className="relative flex justify-end">
+              <div className="flex justify-center w-full px-4">
+                <div className="relative w-full max-w-sm">
+                  <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+                  <input
+                    className="pl-10 pr-3 py-2 border rounded-full w-full hover:shadow-lg focus:shadow-lg focus:outline-0"
+                    type="text"
+                    placeholder="検索"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="m-5 flex flex-wrap justify-center items-center">
-          {filteredUsers.map((user) => (
-            // <Link key={user.id} href={`../staff/record/${user.id}`}>
-            <Link key={user.id} href={`../staff/record/`}>
-              <button
-                // key={user.id}
-                className={
-                  "my-2 mx-5 min-w-[24ch] px-8 p-5 border-2 bg-white border-gray-100 shadow-md rounded-lg hover:border-2 hover:border-kd-sub2-cl overflow-hidden"
-                }
-                title={user.name}
-              >
-                {user.name.length > 8
-                  ? user.name.slice(0, 8) + "..."
-                  : user.name}
-                <br />
-                学籍番号：{user.studentIdNumber}
-              </button>
-            </Link>
-          ))}
+          <div className="m-5 mt-0 py-2 flex flex-wrap justify-center items-center">
+            {filteredUsers.length > 0 ? (
+              filteredUsers.map((user) => (
+                // <Link key={user.id} href={`../staff/record/${user.id}`}>
+                <Link key={user.id} href={`../staff/record/`}>
+                  <button
+                    // key={user.id}
+                    className={
+                      "my-2 mx-5 min-w-[24ch] px-8 p-5 border-2 bg-white border-gray-100 shadow-md rounded-lg hover:border-2 hover:border-kd-sub2-cl overflow-hidden"
+                    }
+                    title={user.name}
+                  >
+                    {user.name.length > 8
+                      ? user.name.slice(0, 8) + "..."
+                      : user.name}
+                    <br />
+                    学籍番号：{user.studentIdNumber}
+                  </button>
+                </Link>
+
+              ))
+            ) : (
+              <SkeletonRow />
+            )
+            }
+          </div>
         </div>
       </div>
     </div>
