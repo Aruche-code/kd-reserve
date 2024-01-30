ee6fd740f73e91d1cb12133e1f8ca92ca747821e
Aruche-code <mo.crew2435@gmail.com>
2024-01-30 15:29:30 +0900

    null check dev

--- app/(root)/staff/record/[id]/page.tsx
+++ app/(root)/staff/record/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import { usePathname } from "next/navigation";
@@ -17,7 +17,7 @@ const Record = () => {
   const [selected, setSelected] = useState<number | null>(null);
 
   const pathname = usePathname();
-  const studentId = pathname.split("/").pop();
+  const studentId = pathname ? pathname.split("/").pop() : "不明";
   const responseRef = useRef<any>(null);
 
   //ローディングフラグ
@@ -31,34 +31,36 @@ const Record = () => {
     qualification: "",
     schoolYear: "",
     tel: "",
-    workLocation: ""
+    workLocation: "",
   });
-  const [studentRecord, setStudentRecord]  = useState<StudentRecordItem[] | null>(null);
+  const [studentRecord, setStudentRecord] = useState<
+    StudentRecordItem[] | null
+  >(null);
 
   const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-  const { data: response, error } = useSWR(`/api/staff/record/${studentId}`, fetcher);
-  const getdata = () => { 
-    if(!response.responseData) {
-        setIsLoading(false);
+  const { data: response, error } = useSWR(
+    `/api/staff/record/${studentId}`,
+    fetcher
+  );
+  const getdata = () => {
+    if (!response.responseData) {
+      setIsLoading(false);
     } else {
-      
-      setnumber(response.responseData[0].email.substring(2, 9))
-      setId(response.responseData[0].id)
-      setName(response.responseData[0].name)
+      setnumber(response.responseData[0].email.substring(2, 9));
+      setId(response.responseData[0].id);
+      setName(response.responseData[0].name);
       const { studentProfile, records } = response.responseData[0];
       setStudentProfile(studentProfile);
       setStudentRecord(records);
-     
     }
-  }
+  };
 
   useEffect(() => {
     if (response) {
-        getdata(); 
+      getdata();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-}, [response]);
-
+  }, [response]);
 
   const data = [
     {
@@ -160,39 +162,58 @@ const Record = () => {
                   )}
                 </span>
               </div>
-              {profileOpen && (
-                studentProfile ? (
-                <div className="p-2 ml-5 mr-5 pb-5 pt-5 text-gray-700 px-2 text-sm md:text-base">
-                  <div className="px-5 mb-1 flex flex-row">
-                    <div className="w-1/3 text-right">学科・学年　/</div>
+              {profileOpen &&
+                (studentProfile ? (
+                  <div className="p-2 ml-5 mr-5 pb-5 pt-5 text-gray-700 px-2 text-sm md:text-base">
+                    <div className="px-5 mb-1 flex flex-row">
+                      <div className="w-1/3 text-right">学科・学年　/</div>
 
-                    <div className="w-2/3 px-4">{studentProfile.department ? studentProfile.department : "未指定"} /  {studentProfile.schoolYear ? studentProfile.schoolYear + "年" : "未指定"}</div>
-                  </div>
-                  <div className="px-5 mb-1 flex flex-row">
-                    <div className="w-1/3 text-right">卒業予定　/</div>
+                      <div className="w-2/3 px-4">
+                        {studentProfile.department
+                          ? studentProfile.department
+                          : "未指定"}{" "}
+                        /{" "}
+                        {studentProfile.schoolYear
+                          ? studentProfile.schoolYear + "年"
+                          : "未指定"}
+                      </div>
+                    </div>
+                    <div className="px-5 mb-1 flex flex-row">
+                      <div className="w-1/3 text-right">卒業予定　/</div>
 
-                    <div className="w-2/3 px-4">{studentProfile.graduationYear ? studentProfile.graduationYear + "年" : "未指定"}</div>
-                  </div>
-                  <div className="px-5 mb-1 flex flex-row">
-                    <div className="w-1/3 text-right">電話番号　/</div>
+                      <div className="w-2/3 px-4">
+                        {studentProfile.graduationYear
+                          ? studentProfile.graduationYear + "年"
+                          : "未指定"}
+                      </div>
+                    </div>
+                    <div className="px-5 mb-1 flex flex-row">
+                      <div className="w-1/3 text-right">電話番号　/</div>
 
-                    <div className="w-2/3 px-4">{studentProfile.tel ? studentProfile.tel: "未指定"}</div>
-                  </div>
-                  <div className="px-5 mb-1 flex flex-row">
-                    <div className="w-1/3 text-right">志望勤務地　/</div>
+                      <div className="w-2/3 px-4">
+                        {studentProfile.tel ? studentProfile.tel : "未指定"}
+                      </div>
+                    </div>
+                    <div className="px-5 mb-1 flex flex-row">
+                      <div className="w-1/3 text-right">志望勤務地　/</div>
 
-                    <div className="w-2/3 px-4">{studentProfile.workLocation ? studentProfile.workLocation: "未指定"}</div>
-                  </div>
-                  <div className="px-5 mb-1 flex flex-row">
-                    <div className="w-1/3 text-right">保有資格　/</div>
+                      <div className="w-2/3 px-4">
+                        {studentProfile.workLocation
+                          ? studentProfile.workLocation
+                          : "未指定"}
+                      </div>
+                    </div>
+                    <div className="px-5 mb-1 flex flex-row">
+                      <div className="w-1/3 text-right">保有資格　/</div>
 
-                    <div className="w-2/3 px-4">{studentProfile.qualification ? studentProfile.qualification: "未指定"}</div>
+                      <div className="w-2/3 px-4">
+                        {studentProfile.qualification
+                          ? studentProfile.qualification
+                          : "未指定"}
+                      </div>
+                    </div>
                   </div>
-                </div>
-                ) :(
-                  null
-                )
-              )}
+                ) : null)}
             </div>
 
             <div className="flex flex-col w-4/5 bg-white rounded-md mt-5 mb-5">
@@ -204,43 +225,44 @@ const Record = () => {
                 <div>
                   <div className="wrapper7">
                     <div className="accordion7">
-                      {studentRecord ? (
-                        studentRecord.map((item:any, i) => (
-                        <div className="item7" key={i}>
-                          <div className="title7" onClick={() => toggle(i)}>
-                            <h2>{item.ymd}　{item.content}</h2>
-                            <span>
-                              {selected === i ? (
-                                <KeyboardArrowUpIcon />
-                              ) : (
-                                <KeyboardArrowDownIcon />
-                              )}
-                            </span>
-                          </div>
-                          <div
-                            className={
-                              selected === i ? "content show7" : "content7"
-                            }
-                          >
-                            <div>
-                              <textarea
-                                className="w-full border-2 mt-2 border-gray-300 rounded-lg p-1 px-5 text-gray-800"
-                                style={{ resize: "none" }}
-                              />
-                              <div className="flex justify-end ">
-                                <button
-                                  type="button"
-                                  className="rounded-lg border border-primary-500 bg-green-300 px-6 py-1 text-center text-sm font-medium text-black shadow-sm transition-all hover:border-primary-700 hover:bg-green-500 hover:text-white"
-                                >
-                                  保存
-                                </button>
+                      {studentRecord
+                        ? studentRecord.map((item: any, i) => (
+                            <div className="item7" key={i}>
+                              <div className="title7" onClick={() => toggle(i)}>
+                                <h2>
+                                  {item.ymd}　{item.content}
+                                </h2>
+                                <span>
+                                  {selected === i ? (
+                                    <KeyboardArrowUpIcon />
+                                  ) : (
+                                    <KeyboardArrowDownIcon />
+                                  )}
+                                </span>
+                              </div>
+                              <div
+                                className={
+                                  selected === i ? "content show7" : "content7"
+                                }
+                              >
+                                <div>
+                                  <textarea
+                                    className="w-full border-2 mt-2 border-gray-300 rounded-lg p-1 px-5 text-gray-800"
+                                    style={{ resize: "none" }}
+                                  />
+                                  <div className="flex justify-end ">
+                                    <button
+                                      type="button"
+                                      className="rounded-lg border border-primary-500 bg-green-300 px-6 py-1 text-center text-sm font-medium text-black shadow-sm transition-all hover:border-primary-700 hover:bg-green-500 hover:text-white"
+                                    >
+                                      保存
+                                    </button>
+                                  </div>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </div>
-                      ))):(
-                        null
-                      )}
+                          ))
+                        : null}
                     </div>
                   </div>
                 </div>
