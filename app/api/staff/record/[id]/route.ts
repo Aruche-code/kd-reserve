4c859ab0bde213a8af9c72b69d1a0edd841c8b80
st0H034 <kd1307152@st.kobedenshi.ac.jp>
2024-01-30 10:50:40 +0900

    add: idで指定された学生の履歴を取得するAPI

--- app/api/staff/record/[id]/route.ts
+++ app/api/staff/record/[id]/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
-//未完成です
-
 // このエンドポイントは、生徒ごとの履歴を取得するAPIです。
 // APIで仕様する検索用キーに生徒のユーザーモデルのオブジェクトIDが必要
 
@@ -11,7 +9,42 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
     // URLの動的な部分からstudentUserIdを抽出
     const studentUserId = req.nextUrl.pathname.split("/").pop();
 
-    return NextResponse.json({ message: "Success" }, { status: 200 });
+    const users = await prisma.user.findMany({
+      where: { id: studentUserId },
+      include: {
+        studentProfile: true,
+        record: true,
+      },
+    });
+
+    const responseData = users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      studentProfile: user.studentProfile
+        ? {
+          department: user.studentProfile?.department,
+          schoolYear: user.studentProfile?.schoolYear,
+          tel: user.studentProfile?.tel,
+          graduationYear: user.studentProfile?.graduationYear,
+          qualification: user.studentProfile?.qualification,
+          workLocation: user.studentProfile?.workLocation,
+        }
+        : null,
+      records: user.record
+        ? user.record.map((record) => ({
+          recordId: record.id,
+          content: record.content,
+          progress: record.progress,
+          ymd: record.ymd,
+        }))
+        : null,
+    }));
+
+    return NextResponse.json(
+      { message: "Success", responseData },
+      { status: 200 }
+    );
   } catch (err) {
     return NextResponse.json({ message: "Server Error", err }, { status: 500 });
   }
